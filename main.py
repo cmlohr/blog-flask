d820013d6530eb214f744e4e816922998ddb0da6
cmlohr <cmlohr@outlook.com>
2021-02-10 00:05:05 -0600

    Last fixes to blog cards and portfolio header

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dogedogedogedoge'
+app.config['SECRET_KEY'] = '5432205846687153cmlohr'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
